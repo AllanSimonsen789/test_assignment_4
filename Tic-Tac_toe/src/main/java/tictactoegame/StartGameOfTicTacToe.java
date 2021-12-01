694fe506ab8e53d0936b18efcd5287f26b93b9df
Ficko05 <filip-f@hotmail.com>
2021-12-01 21:52:29 +0100

    tictactoe 2 player

--- Tic-Tac_toe/src/main/java/tictactoegame/StartGameOfTicTacToe.java
+++ Tic-Tac_toe/src/main/java/tictactoegame/StartGameOfTicTacToe.java
@@ -2,7 +2,29 @@ package tictactoegame;
 
 public class StartGameOfTicTacToe {
 
+
+    private static boolean gameOver = false;
+    private char[][] grid;
+
+
     public static void main(String[] args) {
+        playGroundGrid test = new playGroundGrid();
+
+        System.out.println("2 player Tic Tac Toe game");
+        System.out.println("The placement is indicated by number you write in the console");
+        System.out.println("FX. top left is 1, top right is 3");
+        System.out.println("Bottom left is 7, and bottom right is 9");
+        System.out.println("You get the idea");
+        System.out.println("x always go first");
+
+
+        test.setupGame();
+
+
+
+
 
     }
+
+
 }
--- Tic-Tac_toe/src/test/java/testtictactoe/TestTicTacToe.java
+++ Tic-Tac_toe/src/test/java/testtictactoe/TestTicTacToe.java
@@ -1,18 +1,149 @@
 package testtictactoe;
 
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.stubbing.OngoingStubbing;
+import tictactoegame.StartGameOfTicTacToe;
+import tictactoegame.playGroundGrid;
+
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.StringReader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestTicTacToe {
 
+
+    @Mock
+    playGroundGrid playGroundGrid;
+
+    @Spy
+    char[][] spyGrid;
+
+
+
+    char[][] Grid;
+
+    private char[][] grid;
+
+    //tests if the girds is called
     @Test
-    public void testStartGame() {
+    public void testCreateGrid() {
         // Arrange
-        StartGameOfTicTacToe start = new StartGameOfTicTacToe();
+        playGroundGrid test = mock(playGroundGrid.class);
         // Act
-        start.startTheGame();
+        doNothing().when(test).setupGame();
         // Assert
+        verify(test);
+    }
+
+    @Test
+    public void testItRunsTheConsolOutputVerify() {
+        // Arrange
+        playGroundGrid test = mock(playGroundGrid.class);
+        // Act
+        doNothing().when(test).printBoard(spyGrid);
+        // Assert
+        verify(test);
+    }
+
+    @BeforeEach
+    public void testos() {
+        spyGrid = new char[][]{{' ', '|', ' ', '|', ' '},
+                {'-', '|', '-', '|', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '|', '-', '|', '-'},
+                {' ', '|', ' ', '|', ' '}};
 
     }
+
+
+    @Test
+    public void testPlaceXInTopLeft() {
+        // Arrange
+        playGroundGrid test = mock(playGroundGrid.class);
+        //playGroundGrid test =  new playGroundGrid();
+        // Act
+        grid = new char[][]{{' ', '|', ' ', '|', ' '},
+                {'-', '|', '-', '|', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '|', '-', '|', '-'},
+                {' ', '|', ' ', '|', ' '}};
+        spyGrid = new char[][]{{' ', '|', ' ', '|', ' '},
+                {'-', '|', '-', '|', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '|', '-', '|', '-'},
+                {' ', '|', ' ', '|', ' '}};
+
+        //when(test.whereToPlaceMarker()).thenReturn(1);
+        when(test.placeXMarker(1));
+        String resive = String.valueOf(grid[0][0]);
+        //char[][] result = test.placeXMarker(1, spyGrid);
+
+
+        System.out.println(Grid);
+
+        // Assert
+        assertEquals(resive, 'x');
+    }
+
+    @Test
+    @Disabled
+    public void testWinningScenario() {
+        // Arrange
+        playGroundGrid test = mock(playGroundGrid.class);
+//        playGroundGrid test = new playGroundGrid();
+        // Act
+
+        test.setupGame();
+        when(test.whereToPlaceMarker()).thenReturn(1);
+        when(test.whereToPlaceMarker()).thenReturn(2);
+        when(test.whereToPlaceMarker()).thenReturn(3);
+        char[][] grid;
+        grid = new char[][]{{' ', '|', ' ', '|', ' '},
+                {'-', '|', '-', '|', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '|', '-', '|', '-'},
+                {' ', '|', ' ', '|', ' '}};
+//        test.placeXMarker(1);
+//        test.placeXMarker(2);
+//        test.placeXMarker(3);
+        boolean result = test.winningScenario();
+        System.out.println(result);
+
+        // Assert
+        assertEquals(grid[0][4] = 'x', 'x');
+        assertEquals(grid[0][0] = 'x', 'x');
+        assertEquals(grid[0][2] = 'x', 'x');
+//        assertEquals(true, result);
+    }
+
+
+    //        for (int i = 0 ; i < Grid; i++)
+//            for(int j = 0 ; j < Grid ; j++)
+//            {
+//                if ( grid[i][j] == 88)
+//                {
+//                    Grid this 2 indexes`
+//                    break;
+//                }
+//            }
+
+
+
 }
